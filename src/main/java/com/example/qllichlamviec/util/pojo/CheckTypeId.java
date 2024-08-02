c813cad36637bc3ea9f43bc5b7441b377a892a53
huynhnga <huynhngaa001@gmail.com>
2024-08-02 14:47:15 +0700

    fix thong ke so luong loai lich cua nhan vien hoac don vi

--- src/main/java/com/example/qllichlamviec/util/pojo/CheckTypeId.java
+++ src/main/java/com/example/qllichlamviec/util/pojo/CheckTypeId.java
@@ -5,8 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Component;
 
-public class CheckTypeId {@Autowired
+@Component
+public class CheckTypeId {
+    @Autowired
 private MongoTemplate mongoTemplate;
 
     public String checkIdType(ObjectId id) {
